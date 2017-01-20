bde706a2705cd02a493e07a6525e1c25673adb08
Mats Andresen <mats.andresen@gmx.de>
2017-01-20 21:58:09 +0100

    fix dump

--- scripts/init_database.php
+++ scripts/init_database.php
@@ -2,8 +2,6 @@
 /* parse global.conf */
 $env = parse_ini_file("../config/global.conf", TRUE);
 
-print_r($env);
-
 /* create connection */
 if (!$mysql = mysql_connect($env['mysql']['hostname'], $env['mysql']['username'], $env['mysql']['password'])) {
     die("Connection failed: " . mysql_error());
